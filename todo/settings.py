06a447d33f862b5b4c197b455d132b242ce9d0eb
willymwaura <wilsonmwaura697@gmail.com>
2022-06-05 01:18:34 +0900

    dj_database

--- todo/settings.py
+++ todo/settings.py
@@ -88,6 +88,9 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+import dj_database_url
+db_from_env=dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 '''REST_FRAMEWORK={
     'DEFAULT_AUTHENTICATION_CLASSES':(
