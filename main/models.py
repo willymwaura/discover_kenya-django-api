031917752f1c84a26da902e498385874bc258940
willymwaura <wilsonmwaura697@gmail.com>
2022-06-05 01:31:11 +0900

    models

--- main/models.py
+++ main/models.py
@@ -8,12 +8,12 @@ from pytz import timezone
 class Feature(models.Model):
     region=models.CharField(max_length=100,default='Central')
     title=models.CharField(max_length=100,default='Tsavo')
-    image_url=models.URLField(max_length=30,default="//cdn.weatherapi.com/weather/64x64/day/116.png")
+    image_url=models.URLField(max_length=256,default="//cdn.weatherapi.com/weather/64x64/day/116.png")
     experience=models.CharField(max_length=100,default='i loved it')
     created=models.DateTimeField(auto_now_add=True,blank=True)
     nearby_town=models.CharField(max_length=100,default="nairobi")
     weather_text=models.CharField(max_length=100,default="warm",blank=True)
     degree_celcius=models.IntegerField(default=20,blank=True)
-    weather_url=models.URLField(max_length=30,blank=True)
+    weather_url=models.URLField(max_length=256,blank=True)
     def __str__(self):
         return '{}{}'.format(self.region,self.title)
